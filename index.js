8b8ceb4172498839da9b06f1d607052f0992fa25
william Rodriguez-Ledesma <popolol.wr.wr@gmail.com>
2022-10-14 10:07:56 -0700

    commit tree 6b015f438a2e351fe0fcb2343ac9efc2bb9511ab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+name = "Ralph"   
+cats.push(name); 
+}
+
+function destructivelyPrependCat(name){
+    name = "Bob"
+cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+    return cats
+}
+function appendCat(name){
+    return[...cats, name];
+}
+
+function prependCat(name){
+    return[name,...cats];
+}
+
+function removeFirstCat() {
+return cats.slice(1)
+}
+
+function removeLastCat() {
+    return cats.slice(0,cats.length - 1)
+}
